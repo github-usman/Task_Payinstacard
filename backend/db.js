82a03ab1586405894a3c6f311be616bcbe847b81
github-usman <usmantoyou@gmail.com>
2023-09-07 21:15:06 +0530

    mongoose and Mangod aTlas connet

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,27 @@
 import mongoose from "mongoose";
 
-async function connectToMongo(){
-    try {
-        await mongoose.connect(
-            "mongodb://127.0.0.1:27017/payinsta"
-        )
-        console.log("Database connected");
-    } catch (error) {
-        console.error("Failed to connect to MongoDB :", error);
-    }
+import dotenv from "dotenv";
+dotenv.config();
+
+const dbHost = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbDatabase = process.env.DB_DATABASE;
+
+async function connectToMongo() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${dbHost}:${dbPass}@cluster0.tfxyygf.mongodb.net/${dbDatabase}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // Other MongoDB Atlas options here
+      }
+    );
+    console.log("Connected to MongoDB Atlas successfully");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB Atlas:", error);
+    // Handle the error as needed
+  }
 }
 
-export {connectToMongo};
+export { connectToMongo };
